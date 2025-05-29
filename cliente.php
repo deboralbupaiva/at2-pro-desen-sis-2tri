3f24fe50318054a7e01112bd29721fbb77ee5efd
deboralbupaiva <166133201+deboralbupaiva@users.noreply.github.com>
2025-05-29 12:04:32 -0300

    Add files via upload

---
+++ cliente.php
@@ -0,0 +1,17 @@
+<?php
+
+class Cliente
+{
+    public $nome;
+    public $idade;
+    public $endereco;
+    public $telefone;
+
+    function comprar(){
+        echo "O cliente {$this->nome} realizou uma compra";
+    }
+    function definirNome($nome){
+        $this -> nome = $nome;
+    }
+    function 
+}
